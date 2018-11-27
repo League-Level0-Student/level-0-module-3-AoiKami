31f7846fac04c7148092d98b2ee794cd8ed32005
51299 <51299@0338S-S-R0B6ZHA.sdcs.local>
2018-11-27 14:23:46 -0800

    yo

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -13,18 +13,29 @@ public class BirthdayReminder {
 		// 1. correct the birthdays for your family below
 		String momsBirthday = "January 24th";
 		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String myBirthday = "September 14th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	 String question = JOptionPane.showInputDialog("Which/Whos Birthday?");
 		// 3. Print out what the user typed
-		
+	System.out.println(question);	
 		// 4. if user asked for "mom"
 			//print mom's birthday
+	if(question.equalsIgnoreCase("Mom")) {
+		System.out.println(momsBirthday);
+	}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+	if(question.equalsIgnoreCase("Dad")) {
+		System.out.println(dadsBirthday);
+	}
 		// 6. if user asked for your name
 			// print myBirthday
+	if(question.equalsIgnoreCase("My")) {
+	System.out.println(myBirthday);
+	}else {
+		System.out.println("Your BAD!");
+	}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
 
 	} 
